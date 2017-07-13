2015571945280d6231fb3f1fe7b17103f4325a63
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-07-13 17:04:10 -0500

    rough draft complete project

--- list.js
+++ list.js
@@ -4,9 +4,12 @@
         function subRed(data) {
             $.each(data.data.children.slice(0, 20),
             function (i, item) {
-              $("#reddit-content").append( '<br>' + '<a href="single.html">' + item.data.title + '</a>');
-              $("#reddit-content").append( '<br>' + item.data.url );             
-              $("#reddit-content").append( item.data.title + item.data.url ); //Trying to turn post title into link
+            var a = document.createElement('a')
+            a.href = "single.html?url=" + item.data.permalink
+            a.innerText = item.data.title
+            $("#reddit-content").append(a)
+              // $("#reddit-content").append( '<br>' + item.data.url );             
+              // $("#reddit-content").append( item.data.title ); //Trying to turn post title into link
               $("<img/>").attr("src", item.data.url).appendTo("#reddit-content");
               $("#reddit-content").append( '<hr>' );
             }
@@ -19,18 +22,3 @@
 //         $("<img/>").attr("src", item.data.url).appendTo("#reddit-content");
 //     });
 // });
-//Create click post title 
-    //     var header=document.createElement('h2')
-    //     var div=document.createElement('div')
-    //     document.body.append(header)
-    //     document.body.append(div)
-    //     div.innerText=myData[i].data.selftext
-    //     var appendLink=$(header)
-    //     var a=document.createElement('a')
-    //     a.text=myData[i].data.title
-    //    a.setAttribute('href',single.html?url:myData.data.permalink)
-    //    a.href="single.html?url:" + myData[i].data.permalink
-    //     appendLink.append(a)
-    // }
-// })
-// })
--- single.js
+++ single.js
@@ -1,19 +1,24 @@
-new Request.JSONP({
-    url: 'http://jsfiddle.net/echo/jsonp/',
-    data: {
-        text: 'some text',
-        par1: 'another text'
-    },
-    onSuccess: function(response) {
-        show_response(response, $('post'));
-    }
-}).send();
-
-show_response = function(obj, result) {
-    $H(obj).each(function(v, k) {
-        new Element('li', {
-            text: k + ': ' + v
-        }).inject(result);
+var search = location.search.substring(1);
+var query = search?JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}',
+    function(key, value) { return key===""?value:decodeURIComponent(value) }):{}
+console.log(query)
+$(document).ready(function(){
+    $.ajax({
+        method:"GET",
+        url:'https://www.reddit.com/'+query.url+'.json'
+        
+    })
+    .then(function(success){  
+        console.log(success)
+    //    console.log(myData)
+    //    var myData=success.data.children    
+        var header=document.createElement('h2')
+        var div=document.createElement('div')
+        document.body.append(header)
+        document.body.append(div)
+        header.innerText=success[0].data.children[0].data.title
+        // div.innerText=success[0].data.children[0].data.url
+        $("<img/>").attr("src", success[0].data.children[0].data.url).appendTo(div);
+    })
     });
-    result.highlight();
-};
+
