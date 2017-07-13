a761ea04835d51566b792c28414015fc36b4376f
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-07-13 10:33:12 -0500

    links show and added styles

--- list.js
+++ list.js
@@ -1,9 +1,10 @@
-//Shows title (not clickable), link and image       
+//Shows title (not clickable), link and image
+
     $.getJSON("https://www.reddit.com/r/LanaDelReyPictures/.json?jsonp=?",
-        function foo(data) {
+        function subRed(data) {
             $.each(data.data.children.slice(0, 20),
             function (i, item) {
-              $("#reddit-content").append( '<br>' + item.data.title );
+              $("#reddit-content").append( '<br>' + '<a href="single.html">' + item.data.title + '</a>');
               $("#reddit-content").append( '<br>' + item.data.url );             
               $("#reddit-content").append( item.data.title + item.data.url ); //Trying to turn post title into link
               $("<img/>").attr("src", item.data.url).appendTo("#reddit-content");
@@ -18,27 +19,18 @@
 //         $("<img/>").attr("src", item.data.url).appendTo("#reddit-content");
 //     });
 // });
-// $(document).ready(function(){
-//     $.ajax({
-//         method:"GET",
-//         url:'https://www.reddit.com/r/LanaDelReyPictures/.json?jsonp=?'
-//     })
-//     .then(function(success){  
-//         console.log(success)
-//         var myData=success.data.children
-//     for(var i=0;i<10;i++){
-//         var header=document.createElement('h2')
-//         var div=document.createElement('div')
-//         document.body.append(header)
-//         document.body.append(div)
-//         div.innerText=myData[i].data.selftext
-//         var appendLink=$(header)
-//         var a=document.createElement('a')
-//         a.text=myData[i].data.title
-      //  a.setAttribute('href',single.html?url:myData.data.permalink)
-//        a.href="single.html?url:" + myData[i].data.permalink
-//         appendLink.append(a)
-//     }
-    
+//Create click post title 
+    //     var header=document.createElement('h2')
+    //     var div=document.createElement('div')
+    //     document.body.append(header)
+    //     document.body.append(div)
+    //     div.innerText=myData[i].data.selftext
+    //     var appendLink=$(header)
+    //     var a=document.createElement('a')
+    //     a.text=myData[i].data.title
+    //    a.setAttribute('href',single.html?url:myData.data.permalink)
+    //    a.href="single.html?url:" + myData[i].data.permalink
+    //     appendLink.append(a)
+    // }
 // })
 // })
